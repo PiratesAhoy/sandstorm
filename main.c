251babffc7f5c21369a7150bdf68e70def666e89
Hammie <sam@hammie.be>
2023-04-22 11:01:35 +0200

    Create empty initial project

---
+++ main.c
@@ -0,0 +1,27 @@
+#include "storm-engine\controls.h"
+#include "storm-engine\layers.h"
+#include "storm-engine\interface\messages.h"
+
+object Render;
+
+int iScriptVersion = 54128;
+
+object GameInterface;
+
+void Main() {
+	CreateEntity(&GameInterface, "xinterface");
+	// LayerAddObject(INTERFACE_EXECUTE, &GameInterface, -100);
+	// LayerAddObject(INTERFACE_REALIZE, &GameInterface, -100);
+
+	SendMessage(&GameInterface, "ls", MSG_INTERFACE_INIT, "resource/ini/interfaces/main.ini");
+
+	CreateAndMapControl("ITurnH", 256, 0);
+	CreateAndMapControl("ITurnV", 257, INVERSE_CONTROL);
+}
+
+void CreateAndMapControl(string control_name, int key_code, int state)
+{
+	int control_code = CreateControl(control_name);
+	MapControl(control_code, key_code);
+	SetControlFlags(control_code, state);
+}
